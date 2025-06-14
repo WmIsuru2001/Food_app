cdc5b03c0c35ed52b475deccb7155b99e4a289f3
WmIsuru2001 <155750111+WmIsuru2001@users.noreply.github.com>
2025-06-14 23:08:47 +0530

    update 1

--- lib/screens/splash_screen/getting_started.dart
+++ lib/screens/splash_screen/getting_started.dart
@@ -1,8 +1,15 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-class GettingStarted extends StatelessWidget {
+class GettingStarted extends StatefulWidget {
   const GettingStarted({super.key});
 
+  @override
+  State<GettingStarted> createState() => _GettingStartedState();
+}
+
+class _GettingStartedState extends State<GettingStarted> {
+  CarouselSliderController carouselController = CarouselSliderController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -10,11 +17,71 @@ class GettingStarted extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Container(
-            width: size.width,
-            height: 180,
-            color: const Color(0xffFFD200),
-          )
+          CarouselSlider(
+            options: CarouselOptions(height: size.height / 2),
+            items: [1, 2, 3, 4, 5].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: const BoxDecoration(color: Colors.amber),
+                      child: Text(
+                        'text $i',
+                        style: const TextStyle(fontSize: 16.0),
+                      ));
+                },
+              );
+            }).toList(),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Stack(
+            children: [
+              Container(
+                width: size.width,
+                height: 180,
+                color: const Color(0xffFFD200),
+              ),
+              Positioned(
+                bottom: 65,
+                right: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xffffffff),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: const Text(
+                        "Next",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Skip",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Color(0xff000000),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
